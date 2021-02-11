33b04da175956fe303692eb2b4d8a0b3f35539c5
ShivamSharma5420 <shivam.sharma1.quovantis.com>
2021-02-11 15:26:57 +0530

    requesting server for data and printing on client side

--- public/js/test.js
+++ public/js/test.js
@@ -1,1 +1,42 @@
-console.log("hey js works on server side");
+console.log("hey js is loaded on client side");
+
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+    })
+})
+
+
+
+fetch('http://localhost:3000/weather?address=delhi').then((response) => {
+    response.json().then((data) => {
+        if (data.error)
+            console.log(data.error);
+        else
+            console.log(data.forecast, data.location, data.address);
+    })
+})
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+    //console.log(location);
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error)
+                console.log(data.error);
+            else
+                console.log(data.forecast, data.location, data.address);
+        })
+    })
+
+});
+
+
